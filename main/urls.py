6f781fe6b3417f275cbdd5dbd47dff9121178ffa
hasnaagalal <galalhasnaa4@gmail.com>
2023-03-08 00:47:47 +0200

    third commit

--- main/urls.py
+++ main/urls.py
@@ -14,6 +14,19 @@ urlpatterns = [
     path('vulnerability-filtering-dark/', views.vulnerability_filtering_dark,name='vulnerability-filtering-dark'),  
     path('Custom-filter-light/', views.Custom_filter_light,name='Custom-filter-light'),  
     path('Custom-filter-dark/', views.Custom_filter_dark,name='Custom-filter-dark'),  
+    path('server-Configurations-light/', views.server_Configurations_light,name='server-Configurations-light'),  
+    path('server-Configurations-dark/', views.server_Configurations_dark,name='server-Configurations-dark'),  
+    path('Reporting-light/', views.Reporting_light,name='Reporting-light'),  
+    path('Reporting-dark/', views.Reporting_dark,name='Reporting-dark'),  
+    path('Change-password-light/', views.Change_password_light,name='Change-password-light'),
+    path('Change-password-dark/', views.Change_password_dark,name='Change-password-dark'),
+    path('Users-and-Permissions-light/', views.Users_and_Permissions_light,name='Users-and-Permissions-light'),
+    path('Users-and-Permissions-dark/', views.Users_and_Permissions_dark,name='Users-and-Permissions-dark'),
+    path('login-light/', views.Login,name='login-light'),
+    path('login-dark/', views.Login,name='login-dark'),
+
+
+
 
 
 
--- main/views.py
+++ main/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+
+
 
 def dashboard (request):
     return render (request,'main/dashboard.html',{'title':"dashboard"})
@@ -29,3 +35,57 @@ def Custom_filter_light (request):
 
 def Custom_filter_dark(request):
     return render (request,'main/Custom-filter-dark.html',{'title':"Custom-filter-dark"})
+
+def server_Configurations_light (request):
+    return render (request,'main/server-Configurations-light.html',{'title':"server-Configurations-light"})
+
+def server_Configurations_dark (request):
+    return render (request,'main/server-Configurations-dark.html',{'title':"server-Configurations-dark"})
+
+def Reporting_light (request):
+    return render (request,'main/Reporting-light.html',{'title':"Reporting-light "})
+
+def Reporting_dark (request):
+    return render (request,'main/Reporting-dark.html',{'title':"Reporting-dark "})
+
+def Change_password_light(request):
+    return render (request,'main/Change-password-light.html',{'title':"Change-password-light"})
+
+def Change_password_dark(request):
+    return render (request,'main/Change-password-dark.html',{'title':"Change-password-dark"})
+
+def Users_and_Permissions_light(request):
+    return render (request,'main/Users-and-Permissions-light.html',{'title':"Users-and-Permissions-light"})
+
+def Users_and_Permissions_dark(request):
+    return render (request,'main/Users-and-Permissions-dark.html',{'title':"Users-and-Permissions-dark"})
+
+
+def login_light(request):
+    return render (request,'main/login-light.html',{'title':" login-light"})
+
+def login_dark(request):
+    return render (request,'main/login-dark.html',{'title':" login-dark"})
+
+
+
+def Login(request):
+    if request.method=="POST":
+      username = request.POST.get('username')
+      password = request.POST.get('password')
+      user = authenticate(request, username=username, password=password)
+      if user is not None:
+        login(request, user)
+        return redirect('main:dashboard')
+
+        
+      else:
+        return HttpResponse("Username or passward isnot correct ")
+    return render (request, 'main/login-light.html')
+        
+
+
+
+
+
+

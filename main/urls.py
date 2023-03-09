bb585f4dc6e96cc6c20a47efe46229ef4dc96d06
hasnaagalal <galalhasnaa4@gmail.com>
2023-03-09 18:19:07 +0200

    fourth commit

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,19 @@ from . import views
 app_name='main'
 
 urlpatterns = [
-    path('dashboard/', views.dashboard,name='dashboard'),  
-    path('dashboard-dark/', views.dashboard_dark,name='dashboard-dark'),
+    path('', views.Login,name='login-light'),
+    path('login-dark/', views.Login_dark,name='login-dark'),
+    
+    path('dashboard/', views.view_dashboard,name='dashboard'),  
+    path('dashboard-dark/', views.view_dashboard_dark,name='dashboard-dark'),
+    
+    path('Change-password-light/', views.change_password,name='Change-password-light'),
+    
+    
+    
+    
+    
+    path('Change-password-dark/', views.Change_password_dark,name='Change-password-dark'),
     path('log-analysis-light/', views.log_analysis_light,name='log-analysis-light'),  
     path('log-analysis-dark/', views.log_analysis_dark,name='log-analysis-dark'),  
     path('ip-blocker-light/', views.ip_blocker_light,name='ip-blocker-light'),  
@@ -18,24 +29,12 @@ urlpatterns = [
     path('server-Configurations-dark/', views.server_Configurations_dark,name='server-Configurations-dark'),  
     path('Reporting-light/', views.Reporting_light,name='Reporting-light'),  
     path('Reporting-dark/', views.Reporting_dark,name='Reporting-dark'),  
-    path('Change-password-light/', views.Change_password_light,name='Change-password-light'),
-    path('Change-password-dark/', views.Change_password_dark,name='Change-password-dark'),
     path('Users-and-Permissions-light/', views.Users_and_Permissions_light,name='Users-and-Permissions-light'),
     path('Users-and-Permissions-dark/', views.Users_and_Permissions_dark,name='Users-and-Permissions-dark'),
-    path('login-light/', views.Login,name='login-light'),
-    path('login-dark/', views.Login,name='login-dark'),
 
 
 
 
-
-
- 
-
-
-    
-
-  
 
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render ,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login,update_session_auth_hash
 from django.http import HttpResponse
+from django.contrib import messages
 
 
-
-def dashboard (request):
+@login_required
+def view_dashboard (request):
     return render (request,'main/dashboard.html',{'title':"dashboard"})
 
-def dashboard_dark (request):
+def view_dashboard_dark (request):
     return render (request,'main/dashboard-dark.html',{'title':"dashboard-dark"})
 
 def log_analysis_light (request):
@@ -47,10 +48,59 @@ def Reporting_light (request):
 
 def Reporting_dark (request):
     return render (request,'main/Reporting-dark.html',{'title':"Reporting-dark "})
+    
+    
 
-def Change_password_light(request):
-    return render (request,'main/Change-password-light.html',{'title':"Change-password-light"})
 
+def change_password(request):
+    print(request.method)
+    if request.method == 'POST':
+        old_password = request.POST.get("q_old_Password")
+        new_password = request.POST.get("q_new_Password")
+        confirmed_new_password = request.POST.get("q_confirm_new_Password")
+        print(old_password)
+        if old_password and new_password and confirmed_new_password:
+            if request.user.is_authenticated:
+                user = request.user
+                if not user.check_password(old_password):
+                    messages.warning(request, "your old password is not correct!")
+                    print("your old password is not correct!")
+                else:
+                    if new_password != confirmed_new_password:
+                        messages.warning(request, "your new password not match the confirm password !")
+                        
+                    
+                    elif len(new_password) < 8 or new_password.lower() == new_password or \
+                         new_password.upper() == new_password or new_password.isalnum() or \
+                         not any(i.isdigit() for i in new_password):
+                         
+                         messages.warning(request, "your password is too weak!")
+                        
+
+                    
+
+                    else:
+                        
+                        user.set_password(new_password)
+                        user.save()
+                        update_session_auth_hash(request, user)
+
+                        messages.success(request, "your password has been changed successfuly.!")
+                        return redirect('main:dashboard')
+
+        else:
+            messages.warning(request, " sorry , all fields are required !")
+ 
+
+
+    context = {
+
+    }
+    return render(request, "main/Change-password-light.html", context)
+          
+        
+    
+    
 def Change_password_dark(request):
     return render (request,'main/Change-password-dark.html',{'title':"Change-password-dark"})
 
@@ -59,15 +109,6 @@ def Users_and_Permissions_light(request):
 
 def Users_and_Permissions_dark(request):
     return render (request,'main/Users-and-Permissions-dark.html',{'title':"Users-and-Permissions-dark"})
-
-
-def login_light(request):
-    return render (request,'main/login-light.html',{'title':" login-light"})
-
-def login_dark(request):
-    return render (request,'main/login-dark.html',{'title':" login-dark"})
-
-
 
 def Login(request):
     if request.method=="POST":
@@ -82,6 +123,26 @@ def Login(request):
       else:
         return HttpResponse("Username or passward isnot correct ")
     return render (request, 'main/login-light.html')
+
+def Login_dark(request):
+    if request.method=="POST":
+      username = request.POST.get('username')
+      password = request.POST.get('password')
+      user = authenticate(request, username=username, password=password)
+      if user is not None:
+        login(request, user)
+        return redirect('main:dashboard-dark')
+
+        
+      else:
+        return HttpResponse("Username or passward isnot correct ")
+    return render (request, 'main/login-dark.html')
+
+
+    
+
+
+
         
 
 
